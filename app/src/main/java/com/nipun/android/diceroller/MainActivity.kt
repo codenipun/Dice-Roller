ac16ef89a7b1468389877889d1a5d75c03032fea
nipunjain <nipunjain119@gmail.com>
2021-05-29 19:06:06 +0530

    Initial commit

---
+++ app/src/main/java/com/nipun/android/diceroller/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.nipun.android.diceroller
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val buttonRolled: Button = findViewById(R.id.button)
+
+        buttonRolled.setOnClickListener {
+            Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else->R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = diceRoll.toString()
+    }
+
+    class Dice(private val numSides: Int) {
+        fun roll(): Int {
+            return (1..numSides).random()
+        }
+    }
+}
